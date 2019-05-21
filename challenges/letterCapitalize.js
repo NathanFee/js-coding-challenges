86722248467f67e5d7c8068ca185b19378f47d86
NathanFee <Nathan_Fee@yahoo.com>
2019-05-21 13:38:42 -0400

    Adds answers to letterCapitalize

--- challenges/letterCapitalize.js
+++ challenges/letterCapitalize.js
@@ -4,8 +4,20 @@ capitalize the first letter of each word. Words will be separated by only one
 space.
 */
 
+// let letterCapitalize = function (str) {
+//   let words = str.split(' ')
+//   let capitalized = words.map( word => {
+//     let chars = word.split('')
+//     chars[0] = chars[0].toUpperCase()
+//     return chars.join('')
+//   })
+//   return capitalized.join(' ')
+// }
+
 let letterCapitalize = function (str) {
-  // Answer here 
+  let words = str.split(' ')
+  let capitalizeFirst = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  return capitalizeFirst.join(' ')
 }
 
 module.exports = letterCapitalize
