3a5049588acd9ada40dca47fe71d5a0f0db7db5f
NathanFee <Nathan_Fee@yahoo.com>
2019-05-24 16:42:35 -0400

    Adds new challenge question and spec

---
+++ challenges/repeatedString.js
@@ -0,0 +1,24 @@
+// Lilah has a string,s , of lowercase English letters that she repeated infinitely many times.
+//
+// Given an integer, n, find and print the number of letter a's in the first  letters of Lilah's infinite string.
+//
+// For example, if the string s="abcac" and n=10, the substring we consider is "abcacabcac", the first 10 characters of her infinite string. There are 4 occurrences of 'a' in the substring.
+//
+// Function Description
+//
+// Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of 'a' in the prefix of length n in the infinitely repeating string.
+//
+// repeatedString has the following parameter(s):
+//
+// s: a string to repeat
+// n: the number of characters to consider
+//
+// Print a single integer denoting the number of letter a's in the first letters of the infinite string created by repeating  infinitely many times.
+
+// Sample input s= "aba" n=10, sample output is 7 as the first 10 letters of the infinite sting would be "abaabaabaa" and there are 7 a's in the string
+
+function repeatedString(s, n) {
+  
+}
+
+module.exports = repeatedString
---
+++ spec/challenges/repeatedStringSpec.js
@@ -0,0 +1,19 @@
+describe('Reapeated String:', function () {
+  const repeatedString = require('../../challenges/repeatedString.js')
+
+  const a = "abcac"
+  const b = "aba"
+  const c = "a"
+
+  it('String a', function () {
+    expect(repeatedString(a, 10)).toEqual(4)
+  })
+
+  it('String b', function () {
+    expect(repeatedString(b, 10)).toEqual(7)
+  })
+
+  it('String c', function () {
+    expect(repeatedString(b, 1000000)).toEqual(1000000)
+  })
+})
