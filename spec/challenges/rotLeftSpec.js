04d88c5ee63ee436a72fc988c6e5ad8789395f33
NathanFee <Nathan_Fee@yahoo.com>
2019-05-28 13:07:42 -0400

    Adds spec file for rotLeft test

---
+++ spec/challenges/rotLeftSpec.js
@@ -0,0 +1,13 @@
+describe('Rotate Left:', function () {
+  const rotLeft = require('../../challenges/rotLeft.js')
+
+  const a = [1, 2, 3, 4, 5]
+
+  it('Rotate a left d times', function () {
+    expect(rotLeft(a, 4)).toEqual("5 1 2 3 4")
+  })
+
+  it('Rotate a left d times', function () {
+    expect(rotLeft(a, 2)).toEqual("3 4 5 1 2")
+  })
+})
