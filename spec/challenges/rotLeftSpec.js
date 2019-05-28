a344513c9c88150c012fbab1ba257d95437da9a8
NathanFee <Nathan_Fee@yahoo.com>
2019-05-28 14:11:59 -0400

    Fixes spec to expect an integer array from output

--- spec/challenges/rotLeftSpec.js
+++ spec/challenges/rotLeftSpec.js
@@ -4,10 +4,10 @@ describe('Rotate Left:', function () {
   const a = [1, 2, 3, 4, 5]
 
   it('Rotate a left d times', function () {
-    expect(rotLeft(a, 4)).toEqual("5 1 2 3 4")
+    expect(rotLeft(a, 4)).toEqual([5, 1, 2, 3, 4])
   })
 
   it('Rotate a left d times', function () {
-    expect(rotLeft(a, 2)).toEqual("3 4 5 1 2")
+    expect(rotLeft(a, 2)).toEqual([3, 4, 5, 1, 2])
   })
 })
